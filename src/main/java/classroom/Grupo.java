19bc2a9d471ff6ac4833bc3ccaefc03dc21ca460
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-04-01 20:08:52 -0500

    Update Grupo.java

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -5,7 +5,7 @@ public class Grupo {
     Persona[] estudiantes;
     Persona profesor;
     Asignatura asignatura;
-    final int codigo = 0;
+    final int codigo;
     String horario;
 
     Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
@@ -17,16 +17,22 @@ public class Grupo {
     }
 
     Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
+        this(profesor, asignatura, codigo, horario);
         Persona[] personas = new Persona[cantidadEstudiantes];
-        this(personas, profesor, asignatura, codigo, horario);
+        this.estudiantes=personas;
     }
-
+    Grupo(Persona profesor,Asignatura asignatura,int codigo,String horario){
+        this.profesor=profesor;
+        this.asignatura=asignatura;
+        this.codigo=codigo;
+        this.horario=horario;
+    }
     Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
         this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
+        this.codigo=0;
     }
-
     void cambiarEstudiante(Persona estudianteViejo, Persona estudianteNuevo) {
         for (int i = 0; i < estudiantes.length; i++) {
             if (estudiantes[i].getCedula() == estudianteViejo.getCedula()) {
